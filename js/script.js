ffc32d4fca8eda1ba84af502c99dbaefe4e8ea8f
Serge Simplice Kenne <kenne_serge@mail.ru>
2024-08-28 01:44:21 +0300

    made modal

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 
+$('.header__menu-btn, .header__mobile-menu-btn').click(function () {
+    $('.header__menu').toggle(200, function () {
+        if ($('.header__menu').is(':visible')) {
+          $('.header__menu-btn, .header__mobile-menu-btn').addClass('active');
+          $('.header__mobile-menu-btn > .btn__icon').text('close')
+          $('.header__menu-btn > .btn__icon').css({transform:'rotateZ(180deg)'})
+        } else {
+          $('.header__menu-btn, .header__mobile-menu-btn').removeClass('active');
+          $('.header__mobile-menu-btn > .btn__icon').text('menu');
+          $('.header__menu-btn > .btn__icon').css({ transform: 'rotateZ(0deg)' });
+        }
+    });
+});
 
-const arr = [1, 2, 3]
 
-console.log(arr)
-arr.length = 0;
-console.log(arr)
+$('.header__form-icon').click(function () {
+  $('.header__form-icon').toggleClass('active');
+  $('.header__form').toggleClass('active');
+  $('.header__form-input').toggleClass('active');
+})
